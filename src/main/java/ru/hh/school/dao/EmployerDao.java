758ffc4c8e4fb5758f2607a70aee83c6dc7a979c
Maxim Orlov <orl2011.2011@gmail.com>
2023-12-22 09:38:56 +0300

    Refactored DAOs

--- src/main/java/ru/hh/school/dao/EmployerDao.java
+++ src/main/java/ru/hh/school/dao/EmployerDao.java
@@ -18,7 +18,12 @@ public class EmployerDao extends GenericDao {
    */
   public Employer getEager(int employerId) {
     return getSession()
-        .createQuery("from Employer employer", Employer.class)
+        .createQuery(
+            "select e from Employer e " +
+                "join fetch e.vacancies " +
+                "where e.id = :employerId", Employer.class
+        )
+        .setParameter("employerId", employerId)
         .getSingleResult();
   }
 
